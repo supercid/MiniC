abe720cee01e002bd12d6b794957a8ffea0bb322
cid <alannettto@gmail.com>
2014-09-16 10:44:13 -0300

    MiniC
    Some standard C string functions reimplementations

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include "minic.h"
 
 int main(int argc, char const *argv[]){
+	// Testing my_strlen
 	int j;
-	
 	j =	my_strlen("asdasdasdt");
 	printf("%d\n", j);
 
+	// Testing my_strcmp
 	int k;
-	k= my_strcmp("ab", "abc");
+	k = my_strcmp("ab", "abc");
 	printf("%d\n", k);
+	
 	return 0;
 }
